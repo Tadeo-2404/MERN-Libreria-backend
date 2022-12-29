1a5a43bc9612f428c50656ec263fe24d5aebf8eb
Tadeo Alvarez <tadeo.regalado.alvarez@gmail.com>
2022-12-28 21:44:48 -0600

    correo test

--- helpers/sendEmailConfirmar.js
+++ helpers/sendEmailConfirmar.js
@@ -1,34 +1,38 @@
-import nodemailer from 'nodemailer';
-
+import Sib from 'sib-api-v3-sdk'
+require('dotenv').config()
+const client = Sib.ApiClient.instance
+const apiKey = client.authentications['api-key']
+apiKey.apiKey = process.env.API_KEY
+const tranEmailApi = new Sib.TransactionalEmailsApi()
 const emailConfirmar = async (cliente) => {
-    var transport = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS
-        }
-    });
+    const sender = {
+        email: 'thatanjan@gmail.com',
+        name: 'Anjan',
+    }
+    const receivers = [
+        {
+            email: `${cliente.correo}`,
+        },
+    ]
 
-    var message = {
-        from: "libreria@gdl.com",
-        to: cliente.correo,
-        subject: "Confirmar cuenta en Libreria",
-        html: `
+    await tranEmailApi
+    .sendTransacEmail({
+        sender,
+        to: receivers,
+        subject: 'Confirma tu cuenta en libreria',
+        textContent: `
+        Para poder inciar sesion, confirma tu cuenta.
+        `,
+        htmlContent: `
         <h1>Hola ${cliente.nombre}</h1> 
         <p>Recientemente has creado tu cuenta en Libreria, para confirmar haz click en el siguiente enlace:</p>
         <a href=${process.env.FRONTEND_PORT}/confirmar-cuenta/${cliente.token}>Confirma tu cuenta</a>
         <p>Si desconoces esta actividad, puedes ignorar el correo</p>
-        `
-      };
-
-      await transport.sendMail(message, (error, info) => {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log("Email sent: " + info.response);
-        }
-    });
+                `,
+        params: {
+            role: 'Frontend',
+        },
+    })
 }
 
 export default  emailConfirmar
--- helpers/sendEmailRecuperar.js
+++ helpers/sendEmailRecuperar.js
@@ -1,34 +1,35 @@
-import nodemailer from 'nodemailer';
-
+import Sib from 'sib-api-v3-sdk'
+require('dotenv').config()
+const client = Sib.ApiClient.instance
+const apiKey = client.authentications['api-key']
+apiKey.apiKey = process.env.API_KEY
+const tranEmailApi = new Sib.TransactionalEmailsApi()
 const emailRecuperar = async (cliente) => {
-    var transport = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS
-        }
-    });
+    const sender = {
+        email: 'thatanjan@gmail.com',
+        name: 'Anjan',
+    }
+    const receivers = [
+        {
+            email: `${cliente.correo}`,
+        },
+    ]
 
-    var message = {
-        from: "libreria@gdl.com",
-        to: cliente.correo,
-        subject: "Restablecer contraseña en Libreria",
-        html: `
+    await tranEmailApi
+    .sendTransacEmail({
+        sender,
+        to: receivers,
+        subject: 'Restablecer contraseña en Libreria',
+        textContent: `
+        Confirma tu nueva contraseña
+        `,
+        htmlContent: `
         <h1>Hola ${cliente.nombre}</h1> 
         <p>Recientemente has solicitado restablecer tu contraseña en Libreria, para confirmar haz click en el siguiente enlace:</p>
         <a href=${process.env.FRONTEND_PORT}/olvide-password/${cliente.token}>Restablecer tu contraseña</a>
         <p>Si desconoces esta actividad, puedes ignorar el correo</p>
-        `
-      };
-
-      await transport.sendMail(message, (error, info) => {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log("Email sent: " + info.response);
-        }
-    });
+                `,
+    })
 }
 
 export default  emailRecuperar
